49d3f11e3826f4770cb3b6b2087fe69171f1177c
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-17 17:33:52 +0630

    Add #ask_dest

--- main.rb
+++ main.rb
@@ -9,5 +9,14 @@ def ask_start
   [x, y]
 end
 
+def ask_dest
+  puts 'Type a destination point [x, y]'
+  puts 'Type x: '
+  x = gets.chomp.to_i
+  puts 'Type y: '
+  y = gets.chomp.to_i
+  [x, y]
+end
+
 board = Board.new
-p board.knight_moves(ask_start, [3,3])
+p board.knight_moves(ask_start, ask_dest)
