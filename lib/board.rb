665cfd3f7af7fb7f5308ccbbbae48cac46d0d9ce
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-17 16:29:52 +0630

    Remove unnecessary methods

--- lib/board.rb
+++ lib/board.rb
@@ -62,29 +62,6 @@ class Board
     end
     adjacency_hash
   end
-  # I want like this
-  # hash = {[0, 0] => [[0,1], [0,2]], etc}
-  def gen_neighbors
-    moves = {}
-    @board.each do |cell|
-      gen_moves(cell, moves)
-    end
-    moves
-  end
-
-  def gen_moves(cell, move)
-    x = cell[0]
-    y = cell[1]
-    move[cell] = []
-    move[cell].push([x-1,y+2]) if @board.include?([x-1,y+2])
-    move[cell].push([x+1,y+2]) if @board.include?([x+1,y+2])
-    move[cell].push([x+2,y+1]) if @board.include?([x+2,y+1])
-    move[cell].push([x+2,y-1]) if @board.include?([x+2,y-1])
-    move[cell].push([x+1,y-2]) if @board.include?([x+1,y-2])
-    move[cell].push([x-1,y-2]) if @board.include?([x-1,y-2])
-    move[cell].push([x-2,y-1]) if @board.include?([x-2,y-1])
-    move[cell].push([x-2,y+1]) if @board.include?([x-2,y+1])
-  end
 end
 
 graph = Board.new
