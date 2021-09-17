67103f42e93c67c3b619aab33afe5a4ba2733b1f
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-17 16:11:57 +0630

    Remove #cells and mixin with Creatable module

--- lib/knights.rb
+++ lib/knights.rb
@@ -1,4 +1,7 @@
+require_relative 'board_cells'
+
 class Knight
+  include Creatable
   attr_accessor :src, :legal_moves
 
   def initialize(src)
@@ -20,11 +23,7 @@ class Knight
   end
 
   def choose_legal_moves(array)
-    array.select { |move| cells.include?(move)}
-  end
-
-  def cells
-    (0..7).to_a.product (0..7).to_a
+    array.select { |move| board_cells.include?(move) }
   end
 end
 
